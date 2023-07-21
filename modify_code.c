d5e50ac292ed8f7744903ae07d0810b57b5b9390
Mithil Suthar <136597187+Mithil2603@users.noreply.github.com>
2023-07-21 15:29:30 +0530

    Create modify_code.c

---
+++ modify_code.c
@@ -0,0 +1,1 @@
+//if you feel like something is not appropriate and needs to be change, then please go on and do that in this new file. I hope you will help my code to make it more reliable and effective.
